078b1265ac187119d0295a2ffedcc33038a8f3a8
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-06-28 17:00:29 +0200

    ft_puts with char 8 in parameter

---
+++ src/main.s
@@ -0,0 +1,38 @@
+%define MACH_SYSCALL(nb)		0x2000000 | nb
+%define STDOUT					1
+%define WRITE					4
+
+section .data
+hello:
+	.msg db "Hello World!", 10 ; 10 = \n
+	.len equ $ - hello.msg
+
+section .text
+	global start
+	global _main
+
+start:
+	call _main
+	ret
+
+_ft_puts:		; ft_puts(int string_length, char *str)
+	push rbp
+	mov rbp, rsp
+	mov rdx, [rbp + 16]
+	mov rsi, rdi
+	mov rdi, STDOUT
+	mov rax, MACH_SYSCALL(WRITE)
+	syscall
+	mov rsp, rbp	; Restore the stack and rbp
+	pop rbp
+	ret
+
+_main:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
+	push 13					; parameter
+	mov rdi, hello.msg		; parameter
+	call _ft_puts
+	leave
+	ret
