531d496f7280754b462c9860c57d40cf99b8a93f
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-07-11 15:19:46 +0200

    rm useless comment in ft_bzero

--- src/ft_bzero.s
+++ src/ft_bzero.s
@@ -1,14 +1,5 @@
-; %define MACH_SYSCALL(nb)		0x2000000 | nb
-; %define STDOUT					1
-; %define WRITE					4
-
-; section .data
-; 	msg db "msg = %s", 10, 0
-; 	msg2 db "rsi = %d", 10, 0
-
 section .text
 	global _ft_bzero
-	; extern _printf
 
 _ft_bzero:		; ft_bzero(void *s, int size)
 			push rbp
