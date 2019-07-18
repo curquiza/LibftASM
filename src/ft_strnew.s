c1ef51dea411ef1f80a50cc1ee2a02bfe177d617
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-07-18 19:18:51 +0200

    Fix strnew = bzero after allocation

--- src/ft_strnew.s
+++ src/ft_strnew.s
@@ -1,13 +1,25 @@
 section .text
 	global _ft_strnew
 	extern _ft_memalloc
+	extern _ft_bzero
 
 _ft_strnew:		; char *ft_strnew(size_t size)
 	push rbp
 	mov rbp, rsp
 
+	sub rsp, 64				; save 64bits in function scope
+
 	add rdi, 1
-	call _ft_memalloc 		; ft_memalloc return = ft_strnew return
+	mov [rsp], rdi			; save length
+
+	call _ft_memalloc
+
+	cmp rax, 0				; if malloc failed, leave
+	je .end
+
+	mov rdi, rax			; pointer
+	mov rsi, [rsp]			; len
+	call _ft_bzero			; bzero
 
 	.end:
 		mov rsp, rbp	; Restore the stack and rbp
